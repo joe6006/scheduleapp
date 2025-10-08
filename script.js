b365db2cb5aaccbf64468ae8f6e5048e285aaac2
Joseph Hill <josephhill155@gmail.com>
2025-10-08 11:37:25 -0500

    working on schedule

--- script.js
+++ script.js
@@ -8,6 +8,73 @@ const daysContainer = document.getElementById('days');
 const monthYear = document.getElementById("monthYear");
 let currentDate = new Date();
 
+
+
+
+const addEvent= document.getElementById('addEvent');
+    addEvent.addEventListener('click', () => {
+            //create popup div
+            const createBox = document.createElement('div');
+            createBox.classList.add('popupOverlay');
+            createBox.style.display ='flex ';
+            //create content
+            const createContent= document.createElement('div');
+            createContent.classList.add('popupContent');
+
+            //content child; 
+            const eventForm = document.createElement('form');
+            eventForm.setAttribute('id', 'eventForm');
+            //event name
+            const eventName = document.createElement('label');
+            eventName.textContent = "Event Name: ";
+            const nameInput = document.createElement('input');
+            nameInput.setAttribute('type', 'text');
+            nameInput.setAttribute('name', 'eventName');
+            nameInput.setAttribute('required','true');
+
+            //event time
+            const eventTime = document.createElement('label');
+            eventTime.textContent = "preferred time: ";
+            const timeInput = document.createElement('input');
+            timeInput.setAttribute('type', 'time');
+            timeInput.setAttribute('name', 'preferredTime');
+            timeInput.setAttribute('required','true');
+
+            //date
+             const eventDate = document.createElement('label');
+            eventDate.textContent = "preferred Date: ";
+            const dateInput = document.createElement('input');
+            dateInput.setAttribute('type', 'date');
+            dateInput.setAttribute('name', 'preferredDate');
+            dateInput.setAttribute('required','true');
+
+            //close button
+            const createClose = document.createElement('button');
+            createClose.classList.add('closePopup');
+             createClose.addEventListener('click', () => {
+                document.body.removeChild(createBox);
+                });
+
+            //append
+            
+            createContent.appendChild(createClose);
+            eventForm.appendChild(eventDate);
+            eventForm.appendChild(dateInput);
+            eventForm.appendChild(eventTime);
+            eventForm.appendChild(timeInput);
+            eventForm.appendChild(eventName)
+            eventForm.appendChild(nameInput);
+            createContent.appendChild(eventForm);
+            createBox.appendChild(createContent);
+            document.body.appendChild(createBox);
+                                
+            
+        })
+
+
+
+
+
 function renderCalendar() {
     const year = currentDate.getFullYear();
     const month = currentDate.getMonth();
@@ -31,7 +98,7 @@ function renderCalendar() {
     //set current month and year
     monthYear.textContent = `${monthNames[month]} ${year}`;
 
-
+//set days
     daysContainer.innerHTML = '';
     const firstDay = new Date(year, month, 1).getDay();
     const lastDate = new Date(year, month + 1, 0).getDate();
@@ -49,7 +116,7 @@ function renderCalendar() {
         const childDayText = document.createElement('h3');
         childDayText.textContent = day;
         dayDiv.appendChild(childDayText);
-        
+
         childDayText.classList.add('child-style');
         dayDiv.classList.add('day');
 
@@ -64,6 +131,7 @@ function renderCalendar() {
         
         daysContainer.appendChild(dayDiv);
     }
+    
 }
 
 prevButton.addEventListener('click', () => {
@@ -76,6 +144,13 @@ nextButton.addEventListener('click', () => {
     renderCalendar();
 });
 
+
+
+
+
+
+
 renderCalendar();
+
 
 
